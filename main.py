3af25463558390ef8c1cfc413930672d3ae65a2e
Vanessa Carvalho Lima <73398589+Vnscarli@users.noreply.github.com>
2023-06-20 15:02:31 -0300

    Atualiza os comentários da main

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from classes.Grafo import Grafo
 from classes.Digrafo import Digrafo
 from utils.tools import *
+#Código main do projeto
 
-# Obtendo o caminho do db
+# Obtendo o caminho do BD
 caminhoArquivo, nomeArquivo = ("db/USA-road-d.NY.gr.txt",
                                "USA-road-d.NY.gr") if True else ("db/Teste.txt", "Teste")
 
+#Aqui o usuário vai escolher se quer que os casos teste sejam feitos no grafo ou no digrafo
 tipo = int(input("Escolha qual o tipo de grafo:\n\t1 - Grafo não orientado\t2 - Digrafo\n"))
 
+#Chamando a função com todos os casos teste pedidos no trabalho
 CasosTestes(tipo, caminhoArquivo)
