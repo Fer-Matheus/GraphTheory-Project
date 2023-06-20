9e374829653da26c4fdba253db47485d8ebee15d
Vanessa Carvalho Lima <73398589+Vnscarli@users.noreply.github.com>
2023-06-20 15:00:16 -0300

    Atualiza os comentários de tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -1,10 +1,14 @@
 from classes.Digrafo import Digrafo
 from classes.Grafo import Grafo
+#Código que contém todos os casos testes pedidos no trabalho
 
+#Função que imprime no terminal todos casos testes pedidos
 def CasosTestes(escolha, caminhoArquivo):
     classe = Grafo(caminhoArquivo) if escolha == 1 else Digrafo(caminhoArquivo)
+    #Se for escolhido 1 os casos testes são feitos no grafo, se for 2, serão feitos no digrafo
     tipo = 'G' if escolha == 1 else 'D'
-    
+
+    #A partir do vértice escolhido, imprime todos os casos teste pedidos no trabalho
     vertice = input("Escolha um vértice:\n")
     print("#"*20,end="\n\n")
     print(f'a) O valor de {tipo}.minD(): {classe.minD()}')
